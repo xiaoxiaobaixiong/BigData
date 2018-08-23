a16d9a31cd112d8f8242bf8f8ba90c0acee7f454
TangDuoBuTiao <296206234@qq.com>
2018-08-23 19:16:01 +0800

    first try

---
+++ src/main/java/test/输入未知长度数组.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 输入未知长度数组 {
+
+    public static void main(String args[]) {
+        System.out.print("请输入数组(空格隔开)：");
+        Scanner sc = new Scanner(System.in);
+        String[] nums;
+        nums = sc.nextLine().split(" ");
+        int num[] = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            num[i] = Integer.valueOf(nums[i]);
+        }
+        sc.close();
+        System.out.print(Arrays.toString(num));
+    }
+}
